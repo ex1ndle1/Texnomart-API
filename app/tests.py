8335e8b0b6cbdfc57e22412adea3539b7ff2d52a
ex1ndle1 <muzaffarilzomjonov@gmail.com>
2026-02-08 16:14:01 +0500

    tested JWT comletely

--- app/tests.py
+++ app/tests.py
@@ -5,6 +5,8 @@ from django.urls import reverse
 from .models import ProductCategory
 from django.contrib.auth.models import User
 from . import models
+from rest_framework.test import APITestCase ,APIClient
+
 # Create your tests here.
 
 
@@ -63,22 +65,26 @@ class CommentsAPITest(TestCase):
     def test_post(self):
         self.client.force_login(user=self.user)
         response = self.client.post(self.url , data={'text':'cool' , 'author':self.user.id , 'product':self.product.pk})
-        print(f'errror : {response.data}')
         self.assertEqual(response.status_code ,   status.HTTP_201_CREATED)
 
 
 
-class JWTTest(TestCase):
+class JWTTest(APITestCase):
     def setUp(self):
         self.user = models.UserModel.objects.create_user(username = 'muza'  , password = '123')
         self.get_token = reverse('token_obtain_pair')
-
-
+        self.categories_url = reverse( 'categories_api_view')
+        self.client = APIClient()
+        self.response  = self.client.post(self.get_token  ,  data={'username':'muza' ,  'password':'123'}  ,  format='json')
+        self.access_token  = self.response.data['access']
     def test_get_access_token(self):
-        response  = self.client.post(self.get_token  ,  data={'username':'muza' ,  'password':'123'}  ,  format='json')
-        print(response.data['refresh'])
-        self.assertEqual(response.status_code , status.HTTP_200_OK)
-
+        self.assertEqual(self.response.status_code , status.HTTP_200_OK)
+    
+    def test_get_through_access_token(self):
+         self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {self.access_token}')
+         response =  self.client.post(self.categories_url  , data={'title':'fruits'})
+         self.assertEqual(response.status_code , status.HTTP_201_CREATED)
+        
 
 
 
